fa66301bb26194a7911f3ffb16c48d5839ddfcd7
Laspales <badouel@DESKTOP-0P244VU.localdomain>
2026-05-11 11:14:33 +0200

    add new resume

--- front/src/Components/Footer.jsx
+++ front/src/Components/Footer.jsx
@@ -50,7 +50,7 @@ function Footer() {
                             whileHover={{ scale: 1.1 }}
                             transition={{ type: "spring", stiffness: 300 }}
                         >
-                            <a href="/pdf/CV_BadEpalla_Liora.pdf" target="_blank" rel="noopener noreferrer">
+                            <a href="/pdf/CV_Badouel_Epalla.pdf" target="_blank" rel="noopener noreferrer">
                                 <TbFileCv />
                             </a>
                         </motion.li>
